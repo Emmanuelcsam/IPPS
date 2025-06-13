fbe7956e1d999bb25de1c6a80fbfce58363009a2
Emmanuel Sampson <ecsampson03@gmail.com>
2025-06-13 10:15:33 -0400

    defect cpp

---
+++ build/CMakeFiles/fiber_defect_detector.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fiber_defect_detector.dir/fiber_defect_detector.cpp.o"
+  "CMakeFiles/fiber_defect_detector.dir/fiber_defect_detector.cpp.o.d"
+  "fiber_defect_detector"
+  "fiber_defect_detector.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fiber_defect_detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
